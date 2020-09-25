216da2d6f60e5a21590711aed805082c2fafd98c
quinnfinite <quinn.royston@gmail.com>
2020-09-25 12:31:02 -0700

    updated mapTwo

--- mapTwo.js
+++ mapTwo.js
@@ -1,10 +1,27 @@
 var arr = [2, 6, 78, 101, 4, 34]
 
 //Using map, return an array where each element is a boolean indicating if it's greater than 34
+//Expect: [ false, false, true, true, false, false ]
 
 
-var newArr = arr.map((element)=>{
+//Step 1
+//Write a function that checks if a value is greater than 34
+
+var greaterThanThirtyFour = (element) => {
   return element > 34
-})
+}
+
+//Step 2
+//use map to run that function over each element
+//And assign the return value of map to a variable
+var newArr = arr.map(greaterThanThirtyFour)
+
+
+
+
+//Could also be written as such
+// var newArr = arr.map((element)=>{
+//   return element > 34
+// });
 
 console.log(newArr)
